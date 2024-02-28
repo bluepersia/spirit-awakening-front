c4d5f0156e8bf354f2f547bc28f478cff988c484
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-02-28 10:00:34 -0600

    Email

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,13 +1,16 @@
-import { useContext, useEffect, useState } from 'react';
+import { ChangeEvent, useContext, useEffect, useState } from 'react';
 import styles from './cart.module.css';
 import { AppContext } from '../App';
 import { apiUrl, imgUrl } from '../util/url';
 import BackBtn from '../components/BackBtn';
 import {PayPalScriptProvider, PayPalButtons} from '@paypal/react-paypal-js';
+import validator from 'validator';
 
 export default function Cart () : JSX.Element
 {
     const {cart, removeFromCart, resetCart} = useContext (AppContext);
+    const [email, setEmail] = useState<string>('');
+    const [error, setError] = useState<Error | null>(null);
     const [success, setSuccess] = useState<boolean> (false);
 
     useEffect (() =>
@@ -16,7 +19,19 @@ export default function Cart () : JSX.Element
             resetCart ();
     }, [success])
 
+    function handleInputChange (e:ChangeEvent) : void 
+    {
+        setEmail ((e.target as HTMLInputElement).value);
+    }
+
     function createOrder() {
+
+        if (!validator.isEmail (email))
+        {
+            setError (new Error ('Please provide a valid email'));        
+            return Promise.resolve<string> ('Not an email');
+        }
+        
 
         // Order is created on the server and the order id is returned
         const order = cart.map (product => ({name:product.name, price:product.price}));
@@ -35,7 +50,7 @@ export default function Cart () : JSX.Element
 
           // like product skus and quantities
 
-          body: JSON.stringify(order),
+          body: JSON.stringify({email, order}),
 
         })
 
@@ -97,9 +112,11 @@ export default function Cart () : JSX.Element
                     ))}
                 </tbody>
             </table>
+            <input type='email' name='email' className={styles.email} placeholder='Email addresse' value={email} onChange={handleInputChange}/>
             <PayPalScriptProvider options={{clientId: 'Abp2tpRP2XiipfdYbD7CakHHMD_CMUGtBYwmAkaZJW_bDOzRE3Z26D26cjhd-JTU0THWPM225d_FgTsP', intent:'capture', currency:'EUR'}}>
-                    <PayPalButtons createOrder={createOrder} onApprove={onApprove}/>
+                    <PayPalButtons createOrder={createOrder} onApprove={onApprove} forceReRender={[cart, email]}/>
             </PayPalScriptProvider>
+            {error && <h3 className='error'>{error.message}</h3>}
             </>) : success ? <h3>Vielen dank!</h3> : <h3>Der Warenkorb ist leer.</h3>}
                     
             
